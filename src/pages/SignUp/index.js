1d34668e5f7448afb801d4d3ca1620ccded49cd5
Fábio Vieira <fabioluizvieiraf@gmail.com>
2020-05-13 00:16:02 -0300

    Validações no formulário usando Yup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha precisa ter no mínimo 6 caracteres')
+    .max(55, 'A senha pode ter no máximo 55 caracteres')
+    .required('A senha é obrigatória'),
+});
 
 function SignUp() {
   function handleSubmit(data) {}
@@ -11,7 +23,7 @@ function SignUp() {
     <>
       <img src={logo} alt="GoBarber " />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
