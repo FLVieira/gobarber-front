7af8a838ca52541b05169b54134da0c6ae5aaeb7
Fábio Vieira <fabioluizvieiraf@gmail.com>
2020-05-13 00:01:58 -0300

    Utilizando Unform

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 function SignUp() {
+  function handleSubmit(data) {}
+
   return (
     <>
       <img src={logo} alt="GoBarber " />
 
-      <form>
-        <input placeholder="Nome completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
