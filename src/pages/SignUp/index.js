9976724cd0b209a833594f62148a84e92571b577
Fábio Vieira <fabioluizvieiraf@gmail.com>
2020-05-12 23:41:24 -0300

    Estilização da autenticação

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 function SignUp() {
   return (
-    <div>
-      <h1>SignUp</h1>
-    </div>
+    <>
+      <img src={logo} alt="GoBarber " />
+
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho conta</Link>
+      </form>
+    </>
   );
 }
 
