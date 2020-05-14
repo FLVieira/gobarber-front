4ddd4c09606dfb33c57b40c0a224e60540364954
Fábio Vieira <fabioluizvieiraf@gmail.com>
2020-05-14 02:51:46 -0300

    Página do perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 function Profile() {
+  const { profile } = useSelector((state) => state.user);
+
+  function handleSubmit(data) {}
+
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha "
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button" onClick={() => {}}>
+        Sair do GoBarber
+      </button>
+    </Container>
   );
 }
 
--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -1,14 +1,14 @@
 import produce from 'immer';
 
 const INITIAL_STATE = {
-  user: null,
+  profile: null,
 };
 
 export default function user(state = INITIAL_STATE, action) {
   switch (action.type) {
     case '@auth/SIGN_IN_SUCCESS': {
       return produce(state, (draft) => {
-        draft.user = action.payload.user;
+        draft.profile = action.payload.user;
       });
     }
     default:
