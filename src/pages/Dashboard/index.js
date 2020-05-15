fe94be3a5020d2e64007cf773a3ba9c3f84dd215
Fábio Vieira <fabioluizvieiraf@gmail.com>
2020-05-15 19:47:31 -0300

    Estilização do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
 import api from '~/services/api';
-// import { Container } from './styles';
+
+import { Container, Appointment } from './styles';
 
 function Dashboard() {
-  api.get('appointments');
-
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Appointment past>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Appointment>
+        <Appointment available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Appointment>
+        <Appointment available>
+          <strong>10:00</strong>
+          <span>Em aberto</span>
+        </Appointment>
+        <Appointment>
+          <strong>11:00</strong>
+          <span>Diego Fernandes</span>
+        </Appointment>
+      </ul>
+    </Container>
   );
 }
 
