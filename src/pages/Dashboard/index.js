44cfd1d16e7b791bb691a7e919e29d8280d36e3f
Fábio Vieira <fabioluizvieiraf@gmail.com>
2020-06-09 03:43:33 -0300

    Enviando para o GitHub

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect, useCallback } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import {
   format,
   subDays,
@@ -7,77 +7,57 @@ import {
   setMinutes,
   setSeconds,
   isBefore,
+  isEqual,
+  parseISO,
 } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
-
 import api from '~/services/api';
 
-import { Container, Appointment } from './styles';
+import { Container, Time } from './styles';
 
-const range = [8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19];
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-function Dashboard() {
+export default function Dashboard() {
   const [schedule, setSchedule] = useState([]);
   const [date, setDate] = useState(new Date());
 
-  const comercialDate = useMemo(
+  const dateFormatted = useMemo(
     () => format(date, "d 'de' MMMM", { locale: pt }),
-    [date]
-  );
-
-  const formatDate = useCallback(
-    (index, hour) => {
-      let dayFormatted = String(date.getDate());
-      if (dayFormatted.length === 1) {
-        dayFormatted = `0${dayFormatted}`;
-      }
-      let monthFormatted = String(date.getMonth() + 1);
-      if (monthFormatted.length === 1) {
-        monthFormatted = `0${monthFormatted}`;
-      }
-
-      const dateString = `${date.getFullYear()}-${monthFormatted}-${dayFormatted}`;
-
-      let date1;
-      if (index === 0 || index === 1) {
-        date1 = new Date(`${dateString}T0${hour}:00`);
-      } else {
-        date1 = new Date(`${dateString}T${hour}:00`);
-      }
-
-      const dateByMe = format(date1, "yyyy'-'MM'-'dd'T'HH':00:00.000Z'", {
-        locale: pt,
-      });
-
-      return dateByMe;
-    },
     [date]
   );
 
   useEffect(() => {
     async function loadSchedule() {
-      const response = await api.get('/schedule', {
+      const response = await api.get('schedule', {
         params: { date },
       });
-      const data = range.map((hour, index) => {
-        const fullDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
-        const formattedDate = formatDate(index, hour);
-        // O segredo para consertar o problema das datas aqui é pegar e diminuir 3hrs das datas recebidas
-        // na api e aí sim comparar com as datas no range
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map((hour) => {
+        const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        console.tron.log(
+          response.data.find((a) => isEqual(parseISO(a.date), compareDate))
+        );
+
         return {
           time: `${hour}:00h`,
-          past: isBefore(fullDate, new Date()),
-          appointment: response.data.find(
-            (appointe) => appointe.date === formattedDate
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find((a) =>
+            isEqual(parseISO(a.date), compareDate)
           ),
         };
       });
+
       setSchedule(data);
     }
 
     loadSchedule();
-  }, [date, formatDate]);
+  }, [date]);
 
   function handlePrevDay() {
     setDate(subDays(date, 1));
@@ -91,32 +71,24 @@ function Dashboard() {
     <Container>
       <header>
         <button type="button" onClick={handlePrevDay}>
-          <MdChevronLeft size={36} color="#fff" />
+          <MdChevronLeft size={36} color="#FFF" />
         </button>
-        <strong>{comercialDate}</strong>
+        <strong>{dateFormatted}</strong>
         <button type="button" onClick={handleNextDay}>
-          <MdChevronRight size={36} color="#fff" />
+          <MdChevronRight size={36} color="#FFF" />
         </button>
       </header>
 
       <ul>
-        {schedule.map((appoint) => (
-          <Appointment
-            key={appoint.time}
-            past={appoint.past}
-            available={!appoint.appointment}
-          >
-            <strong>{appoint.time}</strong>
+        {schedule.map((time) => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
             <span>
-              {appoint.appointment
-                ? appoint.appointment.user.name
-                : 'Em aberto'}
+              {time.appointment ? time.appointment.user.name : 'Em aberto'}
             </span>
-          </Appointment>
+          </Time>
         ))}
       </ul>
     </Container>
   );
 }
-
-export default Dashboard;
